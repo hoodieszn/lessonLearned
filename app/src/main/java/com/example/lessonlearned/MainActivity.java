c6a13abce54e0852d923f8d9bb087917a6dd727c
AnmolShah <Anmolshah1997@hotmail.ca>
2019-06-20 18:03:10 -0400

    Created a landing page

--- app/src/main/java/com/example/lessonlearned/MainActivity.java
+++ app/src/main/java/com/example/lessonlearned/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.lessonlearned;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView signUp = findViewById(R.id.textView6);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, Loginpage.class);
+                startActivity(myIntent);
+            }
+        });
+
     }
 }
