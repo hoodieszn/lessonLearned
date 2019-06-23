f5b85fe2268e7a17ef1ec926d011f705dd162cfb
Hamza Usmani <hamzau97@hotmail.com>
2019-06-23 18:13:58 -0400

    Added sorting for price and distance

--- app/src/main/java/com/example/lessonlearned/TutorsActivity.java
+++ app/src/main/java/com/example/lessonlearned/TutorsActivity.java
@@ -1,22 +1,25 @@
 package com.example.lessonlearned;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.TypedValue;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class TutorsActivity extends BaseActivity implements TutorViewAdapter.ItemClickListener{
-    private String sort = "Distance";
+    public ArrayList<String> sortOptions;
+    private int sortSelected;
 
     private String institutionName;
     private int institutionId;
@@ -25,7 +28,7 @@ public class TutorsActivity extends BaseActivity implements TutorViewAdapter.Ite
     private int categoryId;
 
     List<Tutor> tutorList;
-    TutorViewAdapter adapter;
+    TutorViewAdapter tutorListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,29 +46,67 @@ public class TutorsActivity extends BaseActivity implements TutorViewAdapter.Ite
         TextView tutorsTitle = this.findViewById(R.id.tutorsTitle);
         tutorsTitle.setText(institutionName + " " + categoryName + " Tutors in your area");
 
-        Button sortBtn = this.findViewById(R.id.sortBtn);
-        sortBtn.setText(this.sort);
+        sortOptions = new ArrayList<String>() {{
+            add("Price");
+            add("Distance");
+        }};
+
+        final Spinner sortDropdown = this.findViewById(R.id.sortBtn);
+
+        final ArrayAdapter<String> sortAdapter = new ArrayAdapter<String>(this, R.layout.sort_spinner, sortOptions);
+        sortAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sortDropdown.setAdapter(sortAdapter);
+
+        sortSelected = 0;
+        sortDropdown.setSelection(0);
+
+
+        sortDropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                final String selected = parentView.getItemAtPosition(position).toString();
+
+                //default sort is by price
+                Collections.sort(tutorList, new Comparator<Tutor>() {
+                    @Override
+                    public int compare(Tutor lhs, Tutor rhs) {
+                        if (selected == "Distance")
+                            return Double.compare(lhs.distance, rhs.distance);
+                        else
+                            return Double.compare(lhs.price, rhs.price);
+                    }
+                });
+
+                tutorListAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
 
         // Temporarily initializing tutorList with hardcoding tutors
-        tutorList = Arrays.asList(new Tutor("2222222222", "John", "University of Waterloo", Arrays.asList("CS449", "CS370"), 10),
-                new Tutor("3333333333", "Gill", "University of Waterloo", Arrays.asList("CS136", "MATH128"), 20),
-                new Tutor("4444444444", "Bob", "University of Waterloo", Arrays.asList("MATH239", "CS240", "STAT231", "STAT232", "STAT233", "STAT234", "STAT235"), 5),
-                new Tutor("4444444444", "Erin", "University of Waterloo", Arrays.asList("MATH330", "CS250", "STAT241"), 5),
-                new Tutor("4444444444", "Mike", "University of Waterloo", Arrays.asList("MATH249", "CS270", "STAT331"), 15),
-                new Tutor("4444444444", "Samantha", "University of Waterloo", Arrays.asList("MATH239", "CS240", "STAT231"), 8));
+        tutorList = Arrays.asList(new Tutor("2222222222", "John", "University of Waterloo", Arrays.asList("CS449", "CS370"), 10, 3),
+                new Tutor("3333333333", "Gill", "University of Waterloo", Arrays.asList("CS136", "MATH128"), 20, 5),
+                new Tutor("4444444444", "Bob", "University of Waterloo", Arrays.asList("MATH239", "CS240", "STAT231", "STAT232", "STAT233", "STAT234", "STAT235"), 5, 7),
+                new Tutor("4444444444", "Erin", "University of Waterloo", Arrays.asList("MATH330", "CS250", "STAT241"), 5, 1),
+                new Tutor("4444444444", "Mike", "University of Waterloo", Arrays.asList("MATH249", "CS270", "STAT331"), 15, 0.5),
+                new Tutor("4444444444", "Samantha", "University of Waterloo", Arrays.asList("MATH239", "CS240", "STAT231"), 8, 9));
 
         RecyclerView recyclerView = findViewById(R.id.tutors);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
-        adapter = new TutorViewAdapter(this, tutorList);
-        adapter.setClickListener(this);
-        recyclerView.setAdapter(adapter);
+        tutorListAdapter = new TutorViewAdapter(this, tutorList);
+        tutorListAdapter.setClickListener(this);
+        recyclerView.setAdapter(tutorListAdapter);
     }
 
     @Override
     public void onItemClick(View view, int position) {
-        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "You clicked " + tutorListAdapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
     }
 
 }
