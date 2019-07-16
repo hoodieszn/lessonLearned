44826ee06dec609b9b4488e3302aad135b8b6bc7
AnmolShah <Anmolshah1997@hotmail.ca>
2019-07-16 17:51:16 -0400

    Created Registration UI

--- app/src/main/java/com/example/lessonlearned/SignUpActivity.java
+++ app/src/main/java/com/example/lessonlearned/SignUpActivity.java
@@ -1,7 +1,9 @@
 package com.example.lessonlearned;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+
     }
 }
