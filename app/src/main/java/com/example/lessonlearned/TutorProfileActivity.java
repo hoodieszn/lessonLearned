05c42ac922ac82fb691880c862c9a064663d1e62
tirmansidhu <tirmansidhu@gmail.com>
2019-07-18 22:39:25 -0400

    new stuff

--- app/src/main/java/com/example/lessonlearned/TutorProfileActivity.java
+++ app/src/main/java/com/example/lessonlearned/TutorProfileActivity.java
@@ -1,26 +1,27 @@
 package com.example.lessonlearned;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
 import com.example.lessonlearned.Models.TutorPosting;
 import com.example.lessonlearned.Models.User;
 import com.example.lessonlearned.Services.RESTClientRequest;
 import com.example.lessonlearned.Singletons.Context;
-
-import org.json.JSONException;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class TutorProfileActivity extends AppCompatActivity {
+public class TutorProfileActivity extends BaseActivity implements TutorsViewAdapter.ItemClickListener {
 
     private User user;
     private List<TutorPosting> userPostings = new ArrayList<>();
-    public List<TutorPosting> getTutorPostings() {
-        return userPostings;
-    }
+
+    TutorsViewAdapter tutorPostingAdapter;
+    LinearLayoutManager layoutManager;
+    RecyclerView recyclerView;
 
     public void setTutorPostings(List<TutorPosting> userPostings) {
         this.userPostings = userPostings;
@@ -33,16 +34,41 @@ public class TutorProfileActivity extends AppCompatActivity {
 
         user = Context.getUser();
         // Fetch Postings from server
-        try {
-            RESTClientRequest.getTutorPostings(user.getId(), this);
-        }
-        catch (JSONException e){
-            Log.d("JSONException", e.toString());
-        }
+        RESTClientRequest.getTutorPostings(user.getId(), this);
     }
 
     public void populateTutorProfile() {
+        TextView userName = findViewById(R.id.tutorName);
+        userName.setText(user.getName());
+
+        TextView school = findViewById(R.id.school);
+        school.setText(user.getSchoolName());
+
+        TextView phone = findViewById(R.id.phone);
+        phone.setText(user.getPhone());
+
+        initActivePostings();
 
     }
 
+    private void initActivePostings() {
+        layoutManager = new LinearLayoutManager(TutorProfileActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView = findViewById(R.id.tutorPostings);
+
+        tutorPostingAdapter = new TutorsViewAdapter(TutorProfileActivity.this, userPostings);
+        tutorPostingAdapter.setClickListener(TutorProfileActivity.this);
+
+        recyclerView.setAdapter(tutorPostingAdapter);
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Intent tutorProfile = new Intent(getApplicationContext(), TutorPostingActivity.class);
+
+        TutorPosting currentPosting = tutorPostingAdapter.getItem(position);
+        tutorProfile.putExtra("tutorId", currentPosting.getTutorId());
+        tutorProfile.putExtra("postingId", currentPosting.getId());
+        startActivity(tutorProfile);
+    }
 }
